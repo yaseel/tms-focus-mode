e483b07b09ac8810ecdd8ca7d74c1919a41d82dd
Aseel <aseelmahmood12@gmail.com>
2025-02-07 03:56:44 +0100

    Updated for new row class

--- popup.js
+++ popup.js
@@ -31,6 +31,7 @@ function toggleFullscreenLayout(isFullscreenOn) {
 
   if (taskDescription) {
     if (isFullscreenOn) {
+      // Fullscreen layout changes
       document.documentElement.style.width = "100vw";
       document.documentElement.style.overflowX = "hidden";
       document.documentElement.style.margin = "0";
@@ -41,9 +42,18 @@ function toggleFullscreenLayout(isFullscreenOn) {
       document.body.style.padding = "0";
       document.body.style.overflowX = "hidden";
 
-      document.querySelectorAll(".col-md-3, .col-md-4, .navbar, .content-title, .d-flex.justify-content-between.flex-wrap.flex-md-nowrap.align-items-center.pb-2.mb-2.border-bottom").forEach(el => el.remove());
-      document.querySelectorAll(".col-xl-10, .col-md-9").forEach(el => el.style.maxWidth = "97%");
+      // NEW: Switch .row from flex to inline
+      document.querySelectorAll(".row").forEach(el => el.style.display = "inline");
 
+      // Remove side elements
+      document.querySelectorAll(".col-md-3, .col-md-4, .navbar, .content-title, .d-flex.justify-content-between.flex-wrap.flex-md-nowrap.align-items-center.pb-2.mb-2.border-bottom")
+        .forEach(el => el.remove());
+
+      // Adjust main column widths
+      document.querySelectorAll(".col-xl-10, .col-md-9")
+        .forEach(el => el.style.maxWidth = "97%");
+
+      // Fix task description to fill screen
       taskDescription.style.position = "fixed";
       taskDescription.style.top = "0";
       taskDescription.style.width = "100vw";
@@ -53,6 +63,7 @@ function toggleFullscreenLayout(isFullscreenOn) {
       taskDescription.style.padding = "0";
       taskDescription.style.margin = "0";
 
+      // Remove all elements before the first <hr>
       const firstHr = document.querySelector("hr");
       if (firstHr) {
         let sibling = firstHr.previousElementSibling;
@@ -62,14 +73,15 @@ function toggleFullscreenLayout(isFullscreenOn) {
           sibling = prevSibling;
         }
       }
-
     } else {
+      // Revert to normal (reload)
       location.reload();
     }
   }
 }
 
 function checkFullscreenLayout() {
+  // Return true if .col-md-8 has position fixed, meaning we're in fullscreen mode
   const taskDescription = document.querySelector(".col-md-8");
   return taskDescription && taskDescription.style.position === "fixed";
 }
